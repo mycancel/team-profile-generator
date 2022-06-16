059ced2d077a9b3af393675ea74ce59bb1f39002
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-16 17:51:05 -0400

    added github username to all new instances of Engineer; added test for setting a github username; added test for error throw with no github username provided

--- _tests_/Engineer.test.js
+++ _tests_/Engineer.test.js
@@ -1,29 +1,35 @@
 describe('Engineer', () => {
     it('Can create an Engineer instance', () => {
-        const obj = new Engineer('Abi', '1234', 'george@fun.com');
+        const obj = new Engineer('Abi', '1234', 'george@fun.com', 'user');
         expect((typeof (obj))).toBe('object');
     });
 
     it('Can set name', () => {
         const name = 'Abi';
-        const obj = new Engineer(name, '1234', 'george@fun.com');
+        const obj = new Engineer(name, '1234', 'george@fun.com', 'user');
         expect(obj.name).toEqual(name);
     });
 
     it('Can set id', () => {
         const id = '1234';
-        const obj = new Engineer('Abi', id, 'george@fun.com');
+        const obj = new Engineer('Abi', id, 'george@fun.com', 'user');
         expect(obj.id).toEqual(id);
     });
 
     it('Can set email', () => {
         const email = 'george@fun.com';
-        const obj = new Engineer('Abi', '1234', email);
+        const obj = new Engineer('Abi', '1234', email, 'user');
         expect(obj.email).toEqual(email);
     });
 
+    it('Can set github username', () => {
+        const github = 'george@fun.com';
+        const obj = new Engineer('Abi', '1234','george@fun.com', github);
+        expect(obj.github).toEqual(github);
+    });
+
     it('Should throw an error if not provided a name value', () => {
-        const cb = () => new Engineer('', '1234', 'george@fun.com');
+        const cb = () => new Engineer('', '1234', 'george@fun.com', 'user');
         const err = new Error(
             'Please enter a name.'
         );
@@ -31,7 +37,7 @@ describe('Engineer', () => {
     });
 
     it('Should throw an error if not provided an id value', () => {
-        const cb = () => new Engineer('Abi', '', 'george@fun.com');
+        const cb = () => new Engineer('Abi', '', 'george@fun.com', 'user');
         const err = new Error(
             'Please enter an id.'
         );
@@ -39,9 +45,17 @@ describe('Engineer', () => {
     });
     
     it('Should throw an error if not provided an email value', () => {
-        const cb = () => new Engineer('Abi', '1234', '');
+        const cb = () => new Engineer('Abi', '1234', '', 'user');
         const err = new Error(
             'Please enter an email.'
+        );
+        expect(cb).toThrowError(err);
+    });
+
+    it('Should throw an error if not provided a github username', () => {
+        const cb = () => new Engineer('Abi', '1234', 'george@fun.com', '');
+        const err = new Error(
+            'Please enter a GitHub username.'
         );
         expect(cb).toThrowError(err);
     });
@@ -49,7 +63,7 @@ describe('Engineer', () => {
     describe('getName', () => {
         it('Can return name', () => {
             const name = 'Abi';
-            const obj = new Engineer(name, '1234', 'george@fun.com');
+            const obj = new Engineer(name, '1234', 'george@fun.com', 'user');
             const output = obj.getName();
             expect(output).toEqual(name);
         });
@@ -58,7 +72,7 @@ describe('Engineer', () => {
     describe('getId', () => {
         it('Can return id', () => {
             const id = '1234';
-            const obj = new Engineer('Abi', id, 'george@fun.com');
+            const obj = new Engineer('Abi', id, 'george@fun.com', 'user');
             const output = obj.getId();
             expect(output).toEqual(id);
         });
@@ -67,7 +81,7 @@ describe('Engineer', () => {
     describe('getEmail', () => {
         it('Can return email', () => {
             const email = 'george@fun.com';
-            const obj = new Engineer('Abi', '1234', email);
+            const obj = new Engineer('Abi', '1234', email, 'user');
             const output = obj.getEmail();
             expect(output).toEqual(email);
         });
@@ -76,7 +90,7 @@ describe('Engineer', () => {
     describe('getRole', () => {
         it('Can return role', () => {
             const role = 'Engineer'
-            const obj = new Engineer('Abi', '1234', 'george@fun.com');
+            const obj = new Engineer('Abi', '1234', 'george@fun.com', 'user');
             const output = obj.getRole();
             expect(output).toEqual(role);
         });
