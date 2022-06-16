3bdaf430c520cd8fefb31c68dd864f2acabb3fd3
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-16 16:50:22 -0400

    Created a negative test for no name value provided

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -2,8 +2,8 @@ const Employee = require('../lib/Employee');
 
 describe('Employee', () => {
     it('Can create an Employee instance', () => {
-        const employ = new Employee();
-        expect((typeof(employ))).toBe('object');
+        const employ = new Employee('Abi', 1234, 'george@fun.com');
+        expect((typeof (employ))).toBe('object');
     });
 
     it('Can set name', () => {
@@ -13,14 +13,22 @@ describe('Employee', () => {
     });
 
     it('Can set id', () => {
-        const id = 1234;
+        const id = '1234';
         const obj = new Employee('Abi', id);
         expect(obj.id).toEqual(id);
     });
-    
+
     it('Can set email', () => {
         const email = 'george@fun.com';
         const obj = new Employee('Abi', 1234, email);
         expect(obj.email).toEqual(email);
+    });
+
+    it('should throw an error if not provided a name value', () => {
+        const cb = () => new Employee('', 1234, 'george@fun.com');
+        const err = new Error(
+            'Please enter a name.'
+        );
+        expect(cb).toThrowError(err);
     });
 });
