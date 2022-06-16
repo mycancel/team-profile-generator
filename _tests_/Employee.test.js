031e6eeea5121fe5e29ab2cfbb194fb26d3068fa
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-16 17:19:51 -0400

    created tests for methods getName, getId, and getEmail; condensed code for methods in Employee.js; edited test titles for some tests

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -24,7 +24,7 @@ describe('Employee', () => {
         expect(obj.email).toEqual(email);
     });
 
-    it('should throw an error if not provided a name value', () => {
+    it('Should throw an error if not provided a name value', () => {
         const cb = () => new Employee('', '1234', 'george@fun.com');
         const err = new Error(
             'Please enter a name.'
@@ -32,7 +32,7 @@ describe('Employee', () => {
         expect(cb).toThrowError(err);
     });
 
-    it('should throw an error if not provided a name value', () => {
+    it('Should throw an error if not provided an id value', () => {
         const cb = () => new Employee('Abi', '', 'george@fun.com');
         const err = new Error(
             'Please enter an id.'
@@ -40,11 +40,38 @@ describe('Employee', () => {
         expect(cb).toThrowError(err);
     });
     
-    it('should throw an error if not provided a name value', () => {
+    it('Should throw an error if not provided an email value', () => {
         const cb = () => new Employee('Abi', '1234', '');
         const err = new Error(
             'Please enter an email.'
         );
         expect(cb).toThrowError(err);
+    });
+
+    describe('getName', () => {
+        it('Can return name', () => {
+            const name = 'Abi';
+            const obj = new Employee(name, '1234', 'george@fun.com');
+            const output = obj.getName();
+            expect(output).toEqual(name);
+        });
+    });
+    
+    describe('getId', () => {
+        it('Can return id', () => {
+            const id = '1234';
+            const obj = new Employee('Abi', id, 'george@fun.com');
+            const output = obj.getId();
+            expect(output).toEqual(id);
+        });
+    });
+    
+    describe('getEmail', () => {
+        it('Can return email', () => {
+            const email = 'george@fun.com';
+            const obj = new Employee('Abi', '1234', email);
+            const output = obj.getEmail();
+            expect(output).toEqual(email);
+        });
     });
 });
