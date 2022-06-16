44bfd885c5d93712b9263858416d2340fe92fdb8
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-16 16:21:24 -0400

    created Employee test for id and email; changed double quotations to single quotations

--- _tests_/Employee.test.js
+++ _tests_/Employee.test.js
@@ -7,14 +7,20 @@ describe('Employee', () => {
     });
 
     it('Can set name', () => {
-        const name = "Abi";
+        const name = 'Abi';
         const obj = new Employee(name);
         expect(obj.name).toEqual(name);
     });
 
     it('Can set id', () => {
         const id = 1234;
-        const obj = new Employee("Abi", id);
+        const obj = new Employee('Abi', id);
         expect(obj.id).toEqual(id);
+    });
+    
+    it('Can set email', () => {
+        const email = 'george@fun.com';
+        const obj = new Employee('Abi', 1234, email);
+        expect(obj.email).toEqual(email);
     });
 });
