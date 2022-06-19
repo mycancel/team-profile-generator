ab0fb6b2d30e846d321ee3c9caa88a3875f24d56
Mary Cancellieri <mary.cancellieri3@gmail.com>
2022-06-19 17:43:15 -0400

    started index.js; required inquirer and manager file; created managerQs and basic inquirer structure in init()

---
+++ index.js
@@ -0,0 +1,37 @@
+const inquirer = require('inquirer');
+const Manager = require('./lib/Manager');
+
+const managerQs = [
+    {
+        type: 'input',
+        message: 'What is the manager\'s name?',
+        name: 'mName',
+    },
+    {
+        type: 'input',
+        message: 'What is the manager\'s id?',
+        name: 'mId',
+    },
+    {
+        type: 'input',
+        message: 'What is the manager\'s email?',
+        name: 'mEmail',
+    },
+    {
+        type: 'input',
+        message: 'What is the manager\'s office number?',
+        name: 'officeNumber',
+    },
+];
+
+function init() {
+    inquirer
+        .prompt(managerQs)
+        .then((answers) => {
+            // Save managerQs to Manager class
+            const managerProfile = new Manager(answers);
+            // TODO: Create a function for switching to more questions
+        });
+};
+
+init();
